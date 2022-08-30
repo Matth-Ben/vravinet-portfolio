27df94a6f97b035a22a2fbddb24247d326715b34
Matth-Ben <matthias.benoit@outlook.fr>
2022-08-31 00:50:11 +0200

    🐛 [VIDEO] Fix video mobile

--- themes/studio-seven/app/Controllers/FrontPage.php
+++ themes/studio-seven/app/Controllers/FrontPage.php
@@ -6,5 +6,16 @@ use Sober\Controller\Controller;
 
 class FrontPage extends Controller
 {
+  public static function Gallery() {
+    $pageID = get_option('page_on_front');
+    $gallery = get_field('gallery', $pageID);
 
+    foreach ($gallery as $value) {
+      $images[] = Element::image($value['ID'], '1920px');
+    }
+
+    return [
+      'gallery' => $images
+    ];
+  }
 }
--- themes/studio-seven/resources/views/components/video.blade.php
+++ themes/studio-seven/resources/views/components/video.blade.php
@@ -1,9 +1,13 @@
 <div class="c-video">
-  <div class="c-video__video" id="{{ $data['video'] }}"></div>
-  @if ($data['poster'])
-    <div class="c-video__poster">
-      @include('elements/image', ['data' => $data['poster']])
-      <div class="c-video__play" data-video="{{ $data['video'] }}">{{ display_svg('play') }}</div>
-    </div>
+  @if ($data['video'])
+    <div class="c-video__video" id="{{ $data['video'] }}"></div>
   @endif
+  <div class="c-video__poster">
+    @if ($data['poster'])
+      @include('elements/image', ['data' => $data['poster']])
+    @endif
+    @if ($data['video'])
+      <div class="c-video__play" data-video="{{ $data['video'] }}">{{ display_svg('play') }}</div>
+    @endif
+  </div>
 </div>
